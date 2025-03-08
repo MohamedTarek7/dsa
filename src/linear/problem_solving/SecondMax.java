1c1a5c39e657cf3717526ff1bffc376c288daa7a
Mohamed Tarek <mt444458@gmail.com>
2025-03-08 14:27:55 +0300

    update SecondMax.java

--- src/linear/problem_solving/SecondMax.java
+++ src/linear/problem_solving/SecondMax.java
@@ -3,25 +3,24 @@ package linear.problem_solving;
 public class SecondMax {
     
     public static int secondMax(int[] arr) {
-        if (arr == null) {
-            throw new IllegalArgumentException("Array must not be null");
-        }
-        if (arr.length == 1) {
-            return arr[0];
+        if (arr == null || arr.length < 2) {
+            throw new IllegalArgumentException("Array must contain at least two elements.");
         }
 
         int max = arr[0];
+        int second_max = Integer.MIN_VALUE;
+
         for (int num : arr) {
             if(num > max) {
+                second_max = max;
                 max = num;
+            } else if (num > second_max && num != max) {
+                second_max = num;
             }
         }
 
-        int second_max = 0;
-        for (int num : arr) {
-            if (num > second_max && num < max) {
-                second_max = num;
-            }
+        if (second_max == Integer.MIN_VALUE) {
+            throw new IllegalArgumentException("All elements in the array are the same.");
         }
 
         return second_max;
