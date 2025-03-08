819d691f3289e16fa319de6a1332b422fc66be88
Mohamed Tarek <mt444458@gmail.com>
2025-03-09 01:04:10 +0300

    impelemention of moveZero()

--- src/linear/ArrayDS.java
+++ src/linear/ArrayDS.java
@@ -5,7 +5,7 @@ import linear.problem_solving.PrintArray;
 public class ArrayDS {
 
     public static void main(String[] args) {
-        int[] nums = { 0, 1, 0, 4, 12 };
-        PrintArray.printArray(MoveZero.moveZero(nums));
+        int[] nums = { 1, 2, 0, 6, 3, 0 };
+
     }
 }
--- src/linear/MoveZero.java
+++ src/linear/MoveZero.java
@@ -1,10 +1,22 @@
 package linear;
 
 public class MoveZero {
-    public static int[] moveZero(int[] arr) {
-        
-        
 
+    public static int[] moveZero(int[] arr) {
+
+        int length = arr.length;
+        int j = 0;
+
+        for (int i = 0; i < length; i++) {
+            if (arr[i] != 0) {
+                if (i != j) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                j++;
+            }
+        }
         return arr;
     }
 }
