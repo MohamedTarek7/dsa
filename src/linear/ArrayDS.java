2fcb7c2d69296f88cab8d6e353b20b87d284ec1d
Mohamed Tarek - Work PC <mt444458@gmail.com>
2025-03-12 16:12:28 +0300

    -rm

--- src/linear/ArrayDS.java
+++ src/linear/ArrayDS.java
@@ -1,18 +1,9 @@
 package linear;
 
-import linear.problem_solving.PrintArray;
-
 public class ArrayDS {
 
     public static void main(String[] args) {
-        Integer[] arr1 = {1,2,3};
-        Character[] arr2 = {'a', 'b', 'c'};
-        Double[] arr3 = {1.1,2.2,3.3};
-        String[] arr4 = {"what's", "your", "name"};
-
-        PrintArray.printArray(arr1);
-        PrintArray.printArray(arr2);
-        PrintArray.printArray(arr3);
-        PrintArray.printArray(arr4);
+    
+        
     }
 }
--- src/linear/problem_solving/PrintArray.java
+++ src/linear/problem_solving/PrintArray.java
@@ -12,4 +12,14 @@ public class PrintArray {
         System.out.println();
     }
 
+    public static void printArray(int[] Arr) {
+        int length = Arr.length;
+        System.out.print("{");
+        for (int i = 0; i < length; i++) {
+            System.out.print(Arr[i] + (i != (length - 1) ? ", " : ""));
+        }
+        System.out.print("}");
+        System.out.println();
+    }
+
 }
