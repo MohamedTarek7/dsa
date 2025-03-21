0323e53df26fde3ba073e14f3afe419bf9b5e571
Mohamed Tarek - Work PC <mt444458@gmail.com>
2025-03-21 13:23:21 +0300

    -

--- src/linear/singlyLinkedList/Main.java
+++ src/linear/singlyLinkedList/Main.java
@@ -2,16 +2,6 @@ package linear.singlyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        SinglyLinkedList s1 = new SinglyLinkedList();
-        s1.head = new ListNode<>(20);
-
-        SinglyLinkedList s2 = new SinglyLinkedList();
-        s2.head = new ListNode<>("USD");
-        s1.head.next = s2.head;
-
-        SinglyLinkedList s3 = new SinglyLinkedList();
-        s3.head = new ListNode<>('$');
-
-        
+                
     }
 }
