2cb67c5caa31096d904179bcca5a71f1b7381fe3
Mohamed Tarek <mt444458@gmail.com>
2025-07-07 03:34:58 +0300

    nothing much

--- src/linear/singlyLinkedList/Main.java
+++ src/linear/singlyLinkedList/Main.java
@@ -1,40 +1,29 @@
 package linear.singlyLinkedList;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Main {
     // @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        // Scanner input = new Scanner(System.in);
 
         // initialization of the Linked List
         SinglyLinkedList sll = new SinglyLinkedList();
-
-        // Decleration of the Linked List
-        // for (int i = 1; i < 10; i++) {
-        //     sll.insertLast(i);
-        // }
-
-        // Display the content of the Linked List
-
-        int key;
         
         sll.insertLast(1);
         sll.insertLast(8);
         sll.insertLast(10);
         sll.insertLast(16);
         
-        System.out.print("List items: ");
+        // Display the content of the Linked List
+        System.out.print("List items before: ");
         sll.print();
         
-        System.out.print("What item do you want to remove: ");
-        key = input.nextInt();
+        // Code here
 
-        sll.removeNodeByKey(key);
-        System.out.print("List items: ");
+        // Display the content of the Linked List
+        System.out.print("List items after: ");
         sll.print();
 
-        // System.out.println("The nth node from end of the lis is: " + sll.getNthNodeFromEnd(3).data);
- 
     }
 }
